f1c26a13ad5a8dfc35bf0e9f734147e366638ee4
vincbeaulieu <37360448+vincbeaulieu@users.noreply.github.com>
2023-10-23 12:40:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -40,17 +40,27 @@ function displayFileIcon() {
     dropArea.appendChild(fileIcon);
 }
 
+
 submitBtn.addEventListener('click', () => {
     const formData = new FormData();
     formData.append('file', uploadedFile);
     fetch('https://vinc.app.n8n.cloud/webhook-test/1419d5b1-5a0d-4477-a4c9-7500e79a6bac', {
         method: 'POST',
         body: formData
-    }).then(response => {
-        if (response.ok) {
-            alert('File uploaded successfully!');
-        } else {
-            alert('Error uploading file.');
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
         }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        alert('File uploaded successfully!');
+    })
+    .catch(error => {
+        console.log('There was a problem with the fetch operation:', error.message);
+        alert('Error uploading file: ' + error.message);
     });
 });
+
